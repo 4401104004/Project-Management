1fd16f3d3225dff554e11c06ed72ad961872d3fe
Hong Dieu <hongdieupvt2000@gmail.com>
2020-09-24 20:36:26 +0700

    lab1

---
+++ lab1/Test Suites/TS_login.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>404c88f3-4c40-42fe-887d-e48f1c6402a0</testSuiteGuid>
+   <testCaseLink>
+      <guid>94d601b4-79e4-4276-b314-31c25158d080</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC001</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3acbadba-7f64-4012-8d4e-45ec954bea07</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC002</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b26c221a-be60-4039-a9db-dcd496fe7bd6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC003</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>639e4810-cac3-4e45-8356-c8c83366949f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC004</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>250c8a40-c81b-452f-9e91-af66dd042cb2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC005</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
